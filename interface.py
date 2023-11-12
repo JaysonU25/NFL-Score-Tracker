0f3c47361dc31ff0a805c1b65d84bae62c50dc52
JaysonU25 <jaysonurena035@gmail.com>
2023-11-12 11:25:48 -0500

    Added flexibility to name input

--- interface.py
+++ interface.py
@@ -1,29 +1,47 @@
 import score_grabber as scores
+import nfl_name_changer as names
 
 if __name__ == '__main__':
     probe = '''
             Hi, what would you like to view today?
             1. View current available scores
-            2. View the previous week's scores
+            2. View a specific team's score
             3. View a specific week's scores
             4. View player stats
 
             ==> '''
     print(probe, end='')
     user = input()
-    if user == '1':
-        scores.get_current_scores('','')
-    if user == '2':
-        print("Not available yet")
-        #scores.get_current_scores('')
-    if user == '3':
-        print(f"Which week and season would you like to view?")
-        print(f"Week: ", end="")
-        week = input()
-        print("Season: ", end="")
-        season = input()
-        scores.get_current_scores(week, season)
-    if user == '4':
-        print("Not available yet")
+    while user != '0':
+        if user == '1':
+            print()
+            scores.get_current_scores('','')
+        elif user == '2':
+            print("\tWhich team would you like to view? \n\t==> ", end='')
+            desired = input()
+            desired = names.change_name(desired)
+            scores.get_specific_score(desired)
+        elif user == '3':
+            print(f"\nWhich week and season would you like to view?")
+            print(f"Week: ", end="")
+            week = input()
+            print("Season: ", end="")
+            season = input()
+            scores.get_current_scores(week, season)
+        elif user == '4':
+            print("\n\tNot available yet")
+        else:
+            print("\n\tNot valid input")
+        probe = '''
+            What else would you like to view today?
+            1. View current available scores
+            2. View a specific team's score
+            3. View a specific week's scores
+            4. View top player stats for a game
+
+            ==> '''
+        print(probe, end='')
+        user = input()
+    
 
 
--- score_grabber.py
+++ score_grabber.py
@@ -39,21 +39,52 @@ def get_current_scores(week, season):
                 print(f"{W_N.text} {W_S.text}-{L_S.text} {L_N.text}\n")    
     return 0
 
-def get_specific_score(team):
+def get_specific_score(desired):
+    desired = desired.upper()
     response = requests.get(URL)
     parsed = BeautifulSoup(response.content, "html.parser")
+
+    games = parsed.find_all("table", class_="teams")
+
+    for game in games: 
+        teams = game.find_all("tr")
+        first_team = ""
+        first_score = ""
+        second_team = ""
+        second_score = ""
+        for team in teams:
+            if team.find("a") != None:
+                if first_team == "":
+                    first_team = team.find("a").text.upper()
+                    first_score = team.find("td", class_="right").text
+                else: 
+                    second_team = team.find("a").text.upper()
+                    second_score = team.find("td", class_="right").text
+            if first_team == desired and second_team != "":
+                print(f"{first_team} {first_score}-{second_score} {second_team}")
+                return 0
+            elif second_team == desired:
+                print(f"{first_team} {first_score}-{second_score} {second_team}")
+                return 0
+            else:
+                continue
+    print(f"Either you spelt it wrong, or the {desired} had a bye this week!")
+    return 0
+
 
 
 
 if __name__ == "__main__":
     #score = get_current_scores()
     #print(score)
-    print(f"Which week and season would you like to view?")
+    # print(f"Which week and season would you like to view?")
     
-    print(f"Week: ", end="")
-    week = input()
-    print("Season: ", end="")
-    season = input()
-    print()
+    # print(f"Week: ", end="")
+    # week = input()
+    # print("Season: ", end="")
+    # season = input()
+    # print()
     
-    get_current_scores(week, season)
+    # get_current_scores(week, season)
+
+    get_specific_score("New York Jets")
